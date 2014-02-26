7ab9b81415bf808ad5d8c35c0f39156f61dcf512
aaron remick <aaron@remick.net>
2014-02-25 23:51:48 -0800

    User CRUD

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -16,7 +16,7 @@ class BootStrap {
             UserRole.create(user1, userRole, true)
         }
 
-        def user2 = User.findByUsername('admin') ?: new User(username: 'admin', enabled: true, password: 'pass').save(failOnError: true)
+        def user2 = User.findByUsername('jenny') ?: new User(username: 'jenny', enabled: true, password: 'pass').save(failOnError: true)
         if (!user2.authorities.contains(userRole)) {
             UserRole.create(user2, userRole, true)
         }
@@ -24,10 +24,18 @@ class BootStrap {
             UserRole.create(user2, adminRole, true)
         }
 
+        def user3 = User.findByUsername('aaron') ?: new User(username: 'aaron', enabled: true, password: 'pass').save(failOnError: true)
+        if (!user3.authorities.contains(userRole)) {
+            UserRole.create(user3, userRole, true)
+        }
+        if (!user3.authorities.contains(adminRole)) {
+            UserRole.create(user3, adminRole, true)
+        }
+
         // make sure that the database is well set up
-        assert User.count == 2
+        assert User.count == 3
         assert Role.count == 2
-        assert UserRole.count == 3
+        assert UserRole.count == 5
     }
 
     def destroy = {
